bda62fc061d9e5eec2f2bdef02bbf9ceefa3ed0a
Adam McLain <pdxadam@gmail.com>
2024-01-03 06:32:01 -0800

    Working now in cleanDropbox2.py. Implemented OAuth with refresh Tokens.

--- cleanDropbox2.py
+++ cleanDropbox2.py
@@ -9,13 +9,14 @@ import shelve
 # >>> Next up: make it send two contents: successes and failures. Then the sensors would work better
 #scp to copy via ssh
 workingDirectory = "/home/gandalf/dropboxSweeper"
+# workingDirectory = "C:\\Users\\cmsmc\\Documents\\Programming\\dropbox"
 #--mqtt variables
 broker = "mchome.local"
 port = 1883
 topic = "homeassistant/dropbox/"
 appKey = ""
 appSecret = ""
-
+shelfFile = "dbData"
 #--------------------------------------------------------------------------------
 #  Started working toward bringing up multiple dropboxes and transfer locations.
 #    Created a file, but got no further than opening it here(commented out below)
@@ -25,7 +26,7 @@ appSecret = ""
 #os.chdir("/home/pi/Documents/dropbox")
 def getShelveData():
     global appKey, appSecret
-    with shelve.open("dbData") as f:
+    with shelve.open(shelfFile) as f:
         try:
             appKey = f["ak"]
             appSecret = f["as"]
@@ -33,19 +34,19 @@ def getShelveData():
             initShelf()
 def initShelf():
     global appKey, appSecret
-    with shelve.open("dbData") as f:
+    with shelve.open(shelfFile) as f:
         appKey = input("What is the app key?")
         appSecret = input("What is the app secret?")
         #I know I should be doing this with pkce.  We'll see if I can get there.
         f["ak"] = appKey
         f["as"] = appSecret
 def saveRefreshToken(token):
-    with shelve.open("dbData") as f:
+    with shelve.open(shelfFile) as f:
         f["refresh"] = token
 
 def getRefreshToken():
 
-    with shelve.open("dbData") as f:
+    with shelve.open(shelfFile) as f:
         try:
             rTok = f["refresh"]
         except:
@@ -64,39 +65,42 @@ def getToken():
     appKey = f.readline().strip()
     appSecret = f.readline().strip()
 
-    l = f.readline()[:-1]#strip the newline character I couldn't get rid of in file
+    l = f.readline().strip()#strip the newline character I couldn't get rid of in file
     return l
+def oAuthProcess():
+    auth_flow = DropboxOAuth2FlowNoRedirect(appKey, appSecret,token_access_type="offline")
+    authorize_url = auth_flow.start()
+    print("1. Go to: " + authorize_url)
+    print("2. Click \"Allow\" (you might have to log in first).")
+    print("3. Copy the authorization code.")
+    auth_code = input("Enter the authorization code here: ").strip()
+    try:
+        oauth_result = auth_flow.finish(auth_code)
+        print(oauth_result)
+        print("==========")
+    except Exception as e:
+        print('Error: %s' % (e,))
+        exit(1)
+    print("initializing dropbox")
+    refresh = oauth_result.refresh_token
+    
+    return refresh
+
 os.chdir(workingDirectory)
 getShelveData()
 rToken = getRefreshToken()
-if rToken != None:
-    #try to do a refresh token
-    newResult = dropbox.DropboxOAuth2FlowNoRedirect())
+if rToken == None: #we don't have a refresh token to work from, so we have to authenticate
+    #what we ought to do is post a 'need to authenticate' to mqtt, then look in args for an authenticate request and run this
+    mqttPost("Error: authentication needed")
+    rToken = oAuthProcess()
+    saveRefreshToken(rToken)
+
 destLoc = "/mnt/musicShare/"
 destPath = Path(destLoc)
 print (" Oauth process")
-auth_flow = DropboxOAuth2FlowNoRedirect(appKey, appSecret,token_access_type="offline")
-
-authorize_url = auth_flow.start()
-print("1. Go to: " + authorize_url)
-print("2. Click \"Allow\" (you might have to log in first).")
-print("3. Copy the authorization code.")
-auth_code = input("Enter the authorization code here: ").strip()
-try:
-    oauth_result = auth_flow.finish(auth_code)
-    print(oauth_result)
-    print("==========")
-except Exception as e:
-    print('Error: %s' % (e,))
-    exit(1)
 
 
-print("initializing dropbox")
-refresh = oauth_result.refresh_token
-saveRefreshToken(refresh)
-print(getRefreshToken())
-print("^^^^^^")
-dbx = dropbox.Dropbox(oauth_result.access_token)
+dbx = dropbox.Dropbox(app_key=appKey, app_secret=appSecret,oauth2_refresh_token=rToken)
 result = dbx.files_list_folder("/Camera Uploads")
 #print(result.entries)
 count = 0
